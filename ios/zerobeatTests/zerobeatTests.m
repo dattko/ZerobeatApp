d74a3ed030de0c6f3b0f8cf192e95e9b3f623494
Min's Git <99301568+dattko@users.noreply.github.com>
2024-09-17 13:50:26 +0900

    앱 이름 변경

--- ios/zerobeatTests/zerobeatTests.m
+++ ios/zerobeatTests/zerobeatTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ZerobeatAppTests : XCTestCase
+@interface zerobeatTests : XCTestCase
 
 @end
 
-@implementation ZerobeatAppTests
+@implementation zerobeatTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
